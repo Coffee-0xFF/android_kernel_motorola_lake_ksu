f5420759f386989bbcb99d4a2cb5558a2644da84
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-30 01:45:26 +0200

    Revert "build: remove hardcoding to arm defconfigs"
    This reverts commit f115c5122578906500cef0a8045dbee72ce92fe5.

--- defconfig.mk
+++ defconfig.mk
@@ -1,4 +1,4 @@
-DEFCONFIGSRC			:= kernel/arch/$(KERNEL_ARCH)/configs
+DEFCONFIGSRC			:= kernel/arch/arm/configs
 LJAPDEFCONFIGSRC		:= ${DEFCONFIGSRC}/ext_config
 PRODUCT_SPECIFIC_DEFCONFIGS	:= $(DEFCONFIGSRC)/$(KERNEL_DEFCONFIG)
 TARGET_DEFCONFIG		:= $(KERNEL_OUT)/mapphone_defconfig
